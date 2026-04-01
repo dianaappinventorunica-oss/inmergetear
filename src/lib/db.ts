0c67f24880cb5e231099e4a49bc888505187feef
dianaappinventorunica-oss <dianaappinventorunica@gmail.com>
2026-04-01 12:31:51 -0500

    Update db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -6,11 +6,8 @@ const globalForPrisma = globalThis as unknown as {
 
 export const db =
   globalForPrisma.prisma ??
-  new PrismaClient({
-    log: process.env.NODE_ENV === 'development' ? ['query'] : [],
-  })
+  new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
-
 
 
